de63d723f0bc9dd2c81469ef659e5349646c4849
unknown <alice890308@gmail.com>
2020-09-03 15:26:11 +0800

    prefilter

--- makefile
+++ makefile
@@ -11,10 +11,11 @@ init:
 
 local: init
 	go build -o=${BIN_DIR}/alice-scheduler ./cmd/scheduler
-	#go build -o=${_output/bin}/alice-scheduler ./cmd/scheduler
+	#go build -o=_output/bin/alice-scheduler ./cmd/scheduler
 
 build-linux: init
 	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o=${BIN_DIR}/alice-scheduler ./cmd/scheduler
+	#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o=_output/bin/alice-scheduler ./cmd/scheduler
 
 image: build-linux
 	docker build --no-cache . -t alice-scheduler:${TAG}
--- pkg/plugin/plugins.go
+++ pkg/plugin/plugins.go
@@ -6,13 +6,18 @@ import (
 	v1qos "k8s.io/kubernetes/pkg/apis/core/v1/helper/qos"
 	"strconv"
 	"k8s.io/apimachinery/pkg/runtime"
+	"context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const Name = "alice-plugin"
+const Name = "alice-scheduler"
 
-type Plugin struct{}
+type Plugin struct{
+	handle framework.FrameworkHandle
+}
 
 var _ framework.QueueSortPlugin = &Plugin{}
+var _ framework.PreFilterPlugin = &Plugin{}
 
 func (p *Plugin) Name() string {
 	return Name
@@ -54,6 +59,69 @@ func compQOS(p1, p2 *v1.Pod) bool {
 	return p2QOS == v1.PodQOSBestEffort
 }
 
-func New(_ *runtime.Unknown, _ framework.FrameworkHandle) (framework.Plugin, error) {
-	return &Plugin{}, nil
+func (p *Plugin) PreFilter(_ context.Context, _ *framework.CycleState, pod *v1.Pod) *framework.Status {
+	groupName, minAvailable, err := getPodGroupLabels(pod)
+	if groupName == "" {
+		return framework.NewStatus(framework.Success, "Pass PreFilter, no groupName")
+	}
+	if (minAvailable == 0) && (err == nil) {
+		return framework.NewStatus(framework.Success, "Pass PreFilter, no minAvailable")
+	}
+	if err != nil {
+		return framework.NewStatus(framework.Unschedulable, "Failed PreFilter, fail in reading labels")
+	}
+	num := p.getPodNumByGroupName(pod.Namespace, groupName)
+
+	if num >= minAvailable {
+		return framework.NewStatus(framework.Success, "Pass PreFilter")
+	} else {
+		return framework.NewStatus(framework.Unschedulable, "Failed PreFilter")
+	}
+}
+
+func getPodGroupLabels(pod *v1.Pod) (string, int, error) {
+	podGroupName, exist := pod.Labels["podGroup"]
+	if !exist || len(podGroupName) == 0 {
+		return "", 0, nil
+	}
+	minAvailable, exist := pod.Labels["minAvailable"]
+	if !exist || len(minAvailable) == 0 {
+		return "", 0, nil
+	}
+	minNum, err := strconv.Atoi(minAvailable)
+	if err != nil {
+		return "", 0, err
+	}
+	if minNum < 1 {
+		return "", 0, err
+	}
+	return podGroupName, minNum, nil
+}
+
+func (p *Plugin)getPodNumByGroupName(ns string, pg string) int {
+	podList, err := p.handle.ClientSet().CoreV1().Pods(ns).List(metav1.ListOptions{})
+	// podList.Items是一個裝很多v1.pod的陣列
+
+	if err != nil {
+		return 0
+	}
+
+	var num int = 0
+	for _, pod := range podList.Items {
+		if pod.Labels["podGroup"] == pg {
+			num++
+		}
+	}
+	return num
+}
+
+
+func (*Plugin)PreFilterExtensions() framework.PreFilterExtensions {
+	return nil
+}
+
+func New(_ *runtime.Unknown, handle framework.FrameworkHandle) (framework.Plugin, error) {
+	return &Plugin{
+		handle: handle,
+	}, nil
 }
